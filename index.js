d50ef838c8d39aec75217c8ca747aebe202bc6d8
Vishal Pawar <vishalpawar.latur@gmail.com>
2024-04-07 15:57:27 +0530

    Sign In Response Added

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
 
+app.get("/signin", (req, res) => {
+    res.send("We are working on sign in functionality! See you soon");
+});
 
 app.get("/blogs", (req, res) => {
     let q = `select * from blogs`;
